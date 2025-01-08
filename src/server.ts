fe140d9e88d2bcd23fbd9553eb99d0260d5cfed4
Dzmitry Tsaruk <45588750+d-tsaruk@users.noreply.github.com>
2025-01-08 19:41:53 +0300

    TV1-19106: added additional header for gabriel (#9)

--- src/server.ts
+++ src/server.ts
@@ -158,7 +158,7 @@ const server = http.createServer((req, res) => {
   // Add CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Custom-User-Agent');
 
   const router: RouteHandlers = {
     '/healthcheck': {
